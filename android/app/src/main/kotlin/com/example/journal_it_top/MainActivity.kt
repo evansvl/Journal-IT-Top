f0c5caf32395c4398cf58affddf01378b82f0c4b
Nikita <135481463+evansvl@users.noreply.github.com>
2025-02-18 18:51:10 +0300

    Changing from Java to Dart

---
+++ android/app/src/main/kotlin/com/example/journal_it_top/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.journal_it_top
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginPage());
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    final url = Uri.parse('https://proxy.evansvl.ru:8444/api/v2/auth/login');
+    final headers = {
+      'authority': 'msapi.top-academy.ru',
+      'accept': 'application/json, text/plain, */*',
+      'content-type': 'application/json',
+      'origin': 'https://journal.top-academy.ru',
+      'referer': 'https://journal.top-academy.ru/',
+      'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0',
+      // Дополнительные заголовки, если необходимо
+    };
+
+    final payload = {
+      'application_key':
+          '6a56a5df2667e65aab73ce76d1dd737f7d1faef9c52e8b8c55ac75f565d8e8a6',
+      'password': _passwordController.text,
+      'username': _usernameController.text,
+    };
+
+    // Логируем запрос
+    print('Request URL: $url');
+    print('Request Headers: $headers');
+    print('Request Payload: ${jsonEncode(payload)}');
+
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: jsonEncode(payload),
+    );
+
+    // Логируем ответ
+    print('Response Status: ${response.statusCode}');
+    print('Response Body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      print('Request was successful');
+    } else {
+      print('Failed request with status: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _login, child: Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
